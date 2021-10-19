eafa90c8cc2e0bace83962678ec5dac87f61b34c
Yusuf Taiwo Hassan <teehazzan@gmail.com>
2021-10-19 07:22:41 +0100

    added text as parameter

--- app/main.py
+++ app/main.py
@@ -10,9 +10,22 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def sayHello():
-    resp={"message": "Please pass in an appropriate text. E.g ' https://quercode.herokuapp.com/www.virtualcoffee.com'"}
-    return resp
+@app.errorhandler(werkzeug.exceptions.BadRequest)
+def generateqrcode():
+    try:
+        data = request.args.get('text')
+        qr = qrcode.QRCode(
+            version=None,
+            box_size=10,
+            border=2)
+        qr.add_data(data)
+        qr.make(fit=True)
+        fill = request.args.get('fill') or 'black'
+        back_color= request.args.get('back_color') or 'white'
+        img = qr.make_image(fill_color=fill, back_color=back_color)
+        return serve_pil_image(img)
+    except:
+        return {"message":'bad request! Check value of fill and back_color queries'}, 400
 
 def serve_pil_image(pil_img):
     img_io = BytesIO()
