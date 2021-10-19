9c43c4ac444281fc78b61f4ed2eeea5787d35389
Yusuf Taiwo Hassan <teehazzan@gmail.com>
2021-10-19 06:49:20 +0100

    added fix

--- app/main.py
+++ app/main.py
@@ -1,20 +1,39 @@
 # save this as app.py
-from flask import Flask
+from flask import Flask, request
 from flask import send_file
 import qrcode
+from io import BytesIO
+import werkzeug
+from werkzeug.wrappers import response
+
 app = Flask(__name__)
 
 
+@app.route("/")
+def sayHello():
+    resp={"message": "Please pass in an appropriate text. E.g ' https://quercode.herokuapp.com/www.virtualcoffee.com'"}
+    return resp
+
+def serve_pil_image(pil_img):
+    img_io = BytesIO()
+    pil_img.save(img_io, 'JPEG', quality=70)
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg')
+
+@app.errorhandler(werkzeug.exceptions.BadRequest)
 @app.route("/<string:data>")
 def generateqr(data):
-    # Creating an instance of qrcode
-    qr = qrcode.QRCode(
-        version=1,
-        box_size=10,
-        border=5)
-    qr.add_data(data)
-    qr.make(fit=True)
-    img = qr.make_image(fill='black', back_color='white')
-    img.save('qrcode001.png')
-    return send_file('qrcode001.png', mimetype='image/gif')
+    try:
+        qr = qrcode.QRCode(
+            version=None,
+            box_size=10,
+            border=5)
+        qr.add_data(data)
+        qr.make(fit=True)
+        fill = request.args.get('fill') or 'black'
+        back_color= request.args.get('back_color') or 'white'
+        img = qr.make_image(fill_color=fill, back_color=back_color)
+        return serve_pil_image(img)
+    except:
+        return {"message":'bad request! Check value of fill and back_color queries'}, 400
 
